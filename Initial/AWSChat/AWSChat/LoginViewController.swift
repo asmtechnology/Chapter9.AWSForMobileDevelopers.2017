afc244ea07fad43084140f7878eb47ee07372df1
Abhishek Mishra <amishra@asmtechnology.com>
2017-03-07 18:49:31 +0000

    created base project

---
+++ Initial/AWSChat/AWSChat/LoginViewController.swift
@@ -0,0 +1,72 @@
+//
+//  LoginViewController.swift
+//  AWSChat
+//
+//  Created by Abhishek Mishra on 07/03/2017.
+//  Copyright © 2017 ASM Technology Ltd. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSignup(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onLogin(_ sender: Any) {
+        
+    }
+    
+    @IBAction func usernameDidEndOnExit(_ sender: Any) {
+        dismissKeyboard()
+    }
+    
+    @IBAction func passwordDidEndOnExit(_ sender: Any) {
+        dismissKeyboard()
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension LoginViewController : UITextFieldDelegate {
+    
+}
+
+
+extension LoginViewController {
+    
+    fileprivate func dismissKeyboard() {
+        usernameField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+    }
+    
+}
+
